f32f760e1eb4ec0354952e2758482b766abb769f
Madalin Popa <contact@madalinpopa.com>
2025-08-06 19:02:10 +0300

    Update neovim plugins and colorscheme

--- home/.config/nvim/lua/plugins/colorscheme.lua
+++ home/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,33 @@
+-- return {
+--    "rose-pine/neovim",
+--    lazy = false,
+--    priority = 50,
+--     config = function()
+--     -- load the colorscheme here
+--     require("rose-pine").setup({
+--         variant = "auto",
+--         styles = {
+--         bold = true,
+--         italic = true,
+--         transparency = true,
+--         },
+--     })
+--     vim.cmd.colorscheme("rose-pine")
+--   end,
+-- }
+
 return {
-   "rose-pine/neovim",
-   lazy = false,
-   priority = 50,
-    config = function()
-    -- load the colorscheme here
-    require("rose-pine").setup({
-        variant = "auto",
-        styles = {
-        bold = true,
-        italic = true,
-        transparency = true,
-        },
-    })
-    vim.cmd.colorscheme("rose-pine")
+  "bjarneo/ash.nvim",
+  priority = 100,
+  config = function()
+
+    vim.cmd([[colorscheme ash]])
+
+    -- transparent background
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+		vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
+
   end,
 }
