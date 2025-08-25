dbad0da70345c9c15d63601f5789258876a1d3ca
Madalin Popa <contact@madalinpopa.com>
2025-08-25 19:53:24 +0300

    Add jj completions to zsh

--- home/.config/zsh/completions.zsh
+++ home/.config/zsh/completions.zsh
@@ -46,3 +46,10 @@ fi
 if command -v op %> /dev/null; then
     eval "$(op completion zsh)"; compdef _op op
 fi
+
+# ----------------------------------------------------------------------
+# jj
+# ----------------------------------------------------------------------
+if command -v jj &> /dev/null; then
+  source <(jj util completion zsh)
+fi
