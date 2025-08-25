a6c43b9a4488dd5b442ab42ed7311dadd34a3f44
Madalin Popa <contact@madalinpopa.com>
2025-08-25 19:04:41 +0300

    Update neovim colorscheme

--- home/.config/nvim/lua/plugins/colorscheme.lua
+++ home/.config/nvim/lua/plugins/colorscheme.lua
@@ -31,3 +31,21 @@ return {
 
   end,
 }
+
+-- return {
+--       'sainnhe/everforest',
+--       lazy = false,
+--       priority = 1000,
+--       config = function()
+--         -- Optionally configure and load the colorscheme
+--         -- directly inside the plugin declaration.
+--         vim.g.everforest_enable_italic = true
+--         vim.cmd.colorscheme('everforest')
+--
+--     -- transparent background
+-- 		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- 		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- 		vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+-- 		vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
+--       end
+-- }
