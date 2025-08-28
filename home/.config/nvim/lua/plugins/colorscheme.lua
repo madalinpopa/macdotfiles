f0a4f4f8f3e400b63325b980a1e84a6c1d1a4830
Madalin Popa <contact@madalinpopa.com>
2025-08-28 08:28:58 +0300

    Update neovim colorscheme

--- home/.config/nvim/lua/plugins/colorscheme.lua
+++ home/.config/nvim/lua/plugins/colorscheme.lua
@@ -16,21 +16,21 @@
 --   end,
 -- }
 
-return {
-  "bjarneo/ash.nvim",
-  priority = 100,
-  config = function()
-
-    vim.cmd([[colorscheme ash]])
-
-    -- transparent background
-		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-		vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
-		vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
-
-  end,
-}
+-- return {
+--   "bjarneo/ash.nvim",
+--   priority = 100,
+--   config = function()
+--
+--     vim.cmd([[colorscheme ash]])
+--
+--     -- transparent background
+-- 		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- 		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- 		vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+-- 		vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
+--
+--   end,
+-- }
 
 -- return {
 --       'sainnhe/everforest',
@@ -49,3 +49,19 @@ return {
 -- 		vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
 --       end
 -- }
+
+return {
+  "vague2k/vague.nvim",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other plugins
+  config = function()
+    require("vague").setup({})
+    vim.cmd("colorscheme vague")
+
+    -- transparent background
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+		vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
+  end
+}
