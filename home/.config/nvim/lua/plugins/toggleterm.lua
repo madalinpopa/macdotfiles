ed9faed75da1e49d202f0187adc9873ff924fecd
Madalin Popa <contact@madalinpopa.com>
2025-08-29 23:54:15 +0300

    Add toggle term plugin

---
+++ home/.config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,11 @@
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	config = function()
+		require("toggleterm").setup({
+			open_mapping = [[<c-\>]],
+			size = 25,
+			direction = "horizontal",
+		})
+	end,
+}
