5fad0c427519d8ce089ad45d00acb98333d4dcac
Madalin Popa <contact@madalinpopa.com>
2025-08-29 23:57:06 +0300

    Update conform plugin settings

--- home/.config/nvim/lua/plugins/conform.lua
+++ home/.config/nvim/lua/plugins/conform.lua
@@ -11,6 +11,8 @@ return {
 				rust = { "rustfmt", lsp_format = "fallback" },
 				-- Conform will run the first available formatter
 				javascript = { "prettierd", "prettier", stop_after_first = true },
+
+				go = { "goimports", "gofmt" },
 			},
 		})
 	end,
