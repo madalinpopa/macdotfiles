9f8f66d579133d7beda7f3ff5e989f618e29f7a3
Madalin Popa <contact@madalinpopa.com>
2025-08-30 11:32:40 +0300

    Cleanup autocmds

--- home/.config/nvim/lua/config/autocmds.lua
+++ home/.config/nvim/lua/config/autocmds.lua
@@ -1,24 +1,20 @@
 -- auto-format on save
-local lsp_fmt_group_gopls = vim.api.nvim_create_augroup("LspFormattingGroup", {})
+-- Used by conform plugin to format buffers
+local conform_group = vim.api.nvim_create_augroup("ConformGroup", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
-	group = lsp_fmt_group_gopls,
-	callback = function()
-		local gopls = vim.lsp.get_clients({ name = "gopls" })
-
-		if vim.tbl_isempty(gopls) then
-			return
-		end
-
-		vim.lsp.buf.format({ name = "gopls", async = true })
+	group = conform_group,
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
 	end,
 })
 
 -- disable auto comment on new line
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "*",
-  callback = function()
-    vim.opt_local.formatoptions:remove({ 'r', 'o' })
-  end,
+	pattern = "*",
+	callback = function()
+		vim.opt_local.formatoptions:remove({ "r", "o" })
+	end,
 })
 
 -- highlight on yank
@@ -33,20 +29,13 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 -- Return to last edit position when opening files
 local buf_read_group = vim.api.nvim_create_augroup("UserConfig", {})
 vim.api.nvim_create_autocmd("BufReadPost", {
-  group = buf_read_group,
-  callback = function()
-    local mark = vim.api.nvim_buf_get_mark(0, '"')
-    local lcount = vim.api.nvim_buf_line_count(0)
-    if mark[1] > 0 and mark[1] <= lcount then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark)
-    end
-  end,
+	group = buf_read_group,
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
 })
 
--- Used by conform plugin to format buffers
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function(args)
-    require("conform").format({ bufnr = args.buf })
-  end,
-})
