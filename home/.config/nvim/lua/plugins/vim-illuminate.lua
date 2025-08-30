b4c5d39b768055176356170704034a5f10a099e3
Madalin Popa <contact@madalinpopa.com>
2025-08-30 10:54:44 +0300

    Update illuminate config

--- home/.config/nvim/lua/plugins/vim-illuminate.lua
+++ home/.config/nvim/lua/plugins/vim-illuminate.lua
@@ -3,6 +3,9 @@ return {
 	lazy = false,
 	config = function()
 		require("illuminate").configure({})
+
+		vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
+		vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
+		vim.api.nvim_set_hl(0, "IlluminateWordWrite", { link = "Visual" })
 	end,
 }
-
