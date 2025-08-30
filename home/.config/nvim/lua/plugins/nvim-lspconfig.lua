ecf49ef7c738c9565dca6fe4479e772ed34cdfcc
Madalin Popa <contact@madalinpopa.com>
2025-08-30 10:54:44 +0300

    Update gopls config

--- home/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ home/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -41,12 +41,36 @@ return {
 			capabilities = capabilities,
 			settings = {
 				gopls = {
-					semanticTokens = true,
-					analyses = {
-						unusedparams = true,
-						shadow = true,
+					gofumpt = true,
+					codelenses = {
+						gc_details = false,
+						generate = true,
+						regenerate_cgo = true,
+						run_govulncheck = true,
+						test = true,
+						tidy = true,
+						upgrade_dependency = true,
+						vendor = true,
 					},
+					hints = {
+						assignVariableTypes = true,
+						compositeLiteralFields = true,
+						compositeLiteralTypes = true,
+						constantValues = true,
+						functionTypeParameters = true,
+						parameterNames = true,
+						rangeVariableTypes = true,
+					},
+					analyses = {
+						nilness = true,
+						unusedparams = true,
+						unusedwrite = true,
+					},
+					usePlaceholders = true,
+					completeUnimported = true,
 					staticcheck = true,
+					directoryFilters = { "-.git", "-.jj", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+					semanticTokens = true,
 				},
 			},
 		})
