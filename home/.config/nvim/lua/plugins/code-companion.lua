d1a7949fe0b2190886dda33b9c0b9e22f20b58d2
Madalin Popa <contact@madalinpopa.com>
2025-10-13 22:39:57 +0300

    feat(nvim): configure code-companion strategies and adapter

--- home/.config/nvim/lua/plugins/code-companion.lua
+++ home/.config/nvim/lua/plugins/code-companion.lua
@@ -2,9 +2,31 @@ return {
 	"olimorris/codecompanion.nvim",
 	opts = {
 		strategies = {
-			-- Change the default chat adapter and model
 			chat = {
 				adapter = "gemini_cli",
+			},
+			inline = {
+				adapter = "gemini_cli",
+			},
+			cmd = {
+				adapter = "gemini",
+			},
+		},
+		adapters = {
+			acp = {
+				gemini_cli = function()
+					return require("codecompanion.adapters").extend("gemini_cli", {
+						-- commands = {
+						-- 	default = { "gemini", "--experimental-acp" },
+						-- },
+						defaults = {
+							auth_method = "oauth-personal", -- "oauth-personal"|"gemini-api-key"|"vertex-ai"
+						},
+						env = {
+							GOOGLE_CLOUD_PROJECT = "cmd:op read op://Development/gemini/project --no-newline",
+						},
+					})
+				end,
 			},
 		},
 	},
@@ -12,6 +34,6 @@ return {
 		"nvim-lua/plenary.nvim",
 	},
 	keys = {
-		{ "<leader>a", mode = "n", "<cmd>CodeCompanionActions<cr>", desc = "Toggle CodeCompanionActions" },
+		{ "<C-s>", mode = { "n", "v" }, "<cmd>CodeCompanionActions<cr>", desc = "Toggle CodeCompanionActions" },
 	},
 }
