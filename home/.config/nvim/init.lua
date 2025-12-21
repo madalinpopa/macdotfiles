d6711654f1d80289b012a1a52aafb3d324cb90b2
Madalin Popa <contact@madalinpopa.com>
2025-12-21 13:18:20 +0200

    Add new line to neovim config file

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -2,5 +2,5 @@
 -- title : Minimal neovim configuration
 -- author: Madalin Popa
 -- ================================================================================================
-
 require("config")
+
