1ab31a556ae1c0149ebef9eb96ebde47807fdcf6
Madalin Popa <contact@madalinpopa.com>
2025-12-21 13:20:48 +0200

    Update mason config to use mason-lspconfig

--- home/.config/nvim/lua/plugins/mason.lua
+++ home/.config/nvim/lua/plugins/mason.lua
@@ -1,13 +1,25 @@
 return {
-	"mason-org/mason.nvim",
-	opts = {
-		ui = {
-			icons = {
-				package_installed = "✓",
-				package_pending = "➜",
-				package_uninstalled = "✗",
-			},
-		},
-	},
+  "mason-org/mason-lspconfig.nvim",
+  opts = {
+    ensure_installed = { "lua_ls", "gopls", "ty" },
+    automatic_enable = {
+      "lua_ls",
+      "gopls"
+    }
+  },
+  dependencies = {
+    {
+      "mason-org/mason.nvim",
+      opts = {
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      },
+    },
+    "neovim/nvim-lspconfig",
+  },
 }
-
